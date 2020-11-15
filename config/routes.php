8e6dd27995f7cd53a927e0f4dc2bde81053c4d89
Angel X. Moreno <angelxmoreno@gmail.com>
2020-11-15 15:05:36 -0500

    Cached image service

--- config/routes.php
+++ config/routes.php
@@ -63,7 +63,7 @@ Router::scope('/', function (RouteBuilder $routes) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, src/Template/Pages/home.ctp)...
      */
-    $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+    $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'index']);
 
     /*
      * ...and connect the rest of 'Pages' controller's URLs.
@@ -91,7 +91,7 @@ Router::scope('/', function (RouteBuilder $routes) {
      */
     $routes->fallbacks(DashedRoute::class);
     $routes->connect('/images/biid/*', ['controller' => 'ImageService', 'action' => 'byImageId']);
-
+    $routes->connect('/images/bmid/*', ['controller' => 'ImageService', 'action' => 'byMovieId']);
 });
 
 /*
